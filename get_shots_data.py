d0478cd87cbca0a90cdc8696a6d03fad7b9fa3c0
Cezar Babin <nibab@Cezars-MacBook-Pro.local>
2016-03-25 22:33:48 -0400

    Added script to get shots data

---
+++ get_shots_data.py
@@ -0,0 +1,10 @@
+import parse 
+import compare
+
+MOTION_ARRAY = parse.motion_data('motion_files/fahim_11_Char00', "")
+
+FILTERED_ARRAYS = compare.main(MOTION_ARRAY, compare.FT_JOINT_LIST)
+
+#compare.plot_data(FILTERED_ARRAYS)
+
+
